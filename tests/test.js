9bd93763c6f5e4f8f1f62d9d4422b0fe2283358a
Masha <nasonkina@yandex.ru>
2021-01-06 05:22:30 +0300

    test fixed

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,8 @@ import request from 'supertest';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {
-    createReadStream, writeFileSync
+    createReadStream,
+    writeFileSync
 } from 'fs';
 import crypto from 'crypto';
 import http from 'http';
@@ -15,22 +16,28 @@ import getClientRequest from '../getClientRequest.js';
 import finalHandler from '../finalHandler.js';
 import contentType from '../contentType.js';
 
-const app = appSrc(express, bodyParser, createReadStream, writeFileSync, crypto, http, cors, getClientRequest, finalHandler, contentType);
+
+(async () => {
+    const app = appSrc(express, bodyParser, createReadStream, writeFileSync, crypto, http, cors, getClientRequest, finalHandler, contentType);
 
 
-//console.log(app);
+    //console.log(app);
 
-const server = request(app);
-const result = await server.get('/login/');
-//console.log(result.text);//mariianasonkina
-//console.log(result.statusCode);//200
-describe('login endpoint', () => {
-    it('is expected to return text "mariianasonkina"', async () => {
-        const res = await server.get('/login/');
-        expect(res.text).toEqual('mariianasonkina');
+    const server = request(app);
+    //const result = await server.get('/login/');
+    //console.log(result.text);//mariianasonkina
+    //console.log(result.statusCode);//200
+    describe('login endpoint', () => {
+        it('is expected to return text "mariianasonkina"', async () => {
+            const res = await server.get('/login/');
+            expect(res.text).toEqual('mariianasonkina');
+        });
+        it('is expected to return code status 200', async () => {
+            const res = await server.get('/login/');
+            expect(res.statusCode).toEqual(200);
+        })
     });
-    it('is expected to return code status 200', async () => {
-        const res = await server.get('/login/');
-        expect(res.statusCode).toEqual(200);
-    })
-});
+
+
+
+})()
